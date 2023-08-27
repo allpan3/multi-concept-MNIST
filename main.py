5d2aa7522f375c16ff93f16f8ec7d30248cbf11b
Allen Pan <allpan@email.com>
2023-08-27 03:05:39 -0700

    Add a small demo to main.py

--- main.py
+++ main.py
@@ -5,12 +5,24 @@ from model.vsa import MultiConceptMNISTVSA
 from dataset import MultiConceptMNIST
 from torch.utils.data import DataLoader
 from tqdm import tqdm
+from matplotlib import pyplot as plt
+
 vsa = MultiConceptMNISTVSA(dim=100, num_colors=NUM_COLOR, num_pos_x=NUM_POS_X, num_pos_y=NUM_POS_Y)
 train_ds = MultiConceptMNIST("./data/multi-concept-MNIST", vsa, train=True, num_samples=9000, max_num_objects=3, num_pos_x=NUM_POS_X, num_pos_y=NUM_POS_Y, num_colors=NUM_COLOR)
-test_ds = MultiConceptMNIST("./data/multi-concept-MNIST", vsa, train=False, num_samples=900, max_num_objects=3, num_pos_x=NUM_POS_X, num_pos_y=NUM_POS_Y, num_colors=NUM_COLOR)
+test_ds = MultiConceptMNIST("./data/multi-concept-MNIST", vsa, train=False, num_samples=3, max_num_objects=3, num_pos_x=NUM_POS_X, num_pos_y=NUM_POS_Y, num_colors=NUM_COLOR)
 
 train_ld = DataLoader(train_ds, batch_size=1, shuffle=True)
 test_ld = DataLoader(test_ds, batch_size=1, shuffle=False)
 
-# for samples, labels in tqdm(train_ld, desc="Training"):
 
+# images in tensor([B, H, W, C])
+# labels in [{'pos_x': tensor, pos_y: tensor, color: tensor, digit: tensor}, ...]
+# targets in VSATensor([B, D])
+for images, labels, targets in tqdm(test_ld, desc="Test"):
+    plt.figure()
+    plt.imshow(images[0])
+    print(labels)
+    print(targets)
+    print()
+
+plt.show()
---
+++ model/nn.py
@@ -0,0 +1,5 @@
+import torch.nn as nn
+
+class MultiConceptMNISTNN(nn.Module):
+    def __init__(self):
+        super(MultiConceptMNISTNN, self).__init__()
